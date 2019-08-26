0c985d42e29882a1f5f432ab64714f9ffeeec0d1
fabiannunes12@gmail.com <fabiannunes12@gmail.com>
2019-08-26 19:12:45 +0100

    Updates

---
+++ precache-manifest.6696302c9e274e87fcb276b79037fb70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b7071b063120d1f1fdb2e64c8c458d",
+    "url": "https//fabian-nunes.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e0ae0d4062d511421274",
+    "url": "https//fabian-nunes.github.io/monsters-rolodex/static/css/main.6eeada8d.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "https//fabian-nunes.github.io/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "e0ae0d4062d511421274",
+    "url": "https//fabian-nunes.github.io/monsters-rolodex/static/js/main.83b8dc24.chunk.js"
+  },
+  {
+    "revision": "6307f65e613177dbb34c",
+    "url": "https//fabian-nunes.github.io/monsters-rolodex/static/js/runtime~main.ec1f98d0.js"
+  }
+]);
